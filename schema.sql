f3c88486ae031e33234e82e8fdc606005481a613
milen-ale <milenalebel2@gmail.com>
2022-10-19 06:25:56 -0700

    add queries to the data base

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer,
+    id integer primary key,
     name varchar(100),
     date_of_birth date,
     escape_attemps integer,
