912740c2ceb5f01b4db21e7a30065c1fcfe2e994
milen-ale <milenalebel2@gmail.com>
2022-10-20 08:39:38 -0700

    create owner and species tabel and modifiy

--- data.sql
+++ data.sql
@@ -20,3 +20,49 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (10, 'Blossom', '1998-10-13', 3, TRUE, 17);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+--INSERT data into ownera table
+INSERT INTO owners(id, full_name, age) VALUES (1, sam smith, 31);
+INSERT INTO owners (id, full_name, age) VALUES (Jennifer Orwell, 19);
+INSERT INTO owners (id, full_name, age) VALUES (Melody Pond, 77);
+INSERT INTO owners (id, full_name, age) VALUES (Dean Winchester , 14);
+INSERT INTO owners (id, full_name, age) VALUES (5,Jodie Whittaker, 38);
+
+--INSERT data into species table
+   INSERT INTO species (name) VALUES ('Pokemon');
+
+   INSERT INTO species (name) VALUES ('Digimon');
+
+   INSERT INTO species (name) VALUES ('Pokemon'),('Digimon');
+
+--Modify your inserted animals so it includes the species_id value
+  --If the name ends in "mon" it will be Digimon
+     UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+     --All other animals are Pokemon
+     UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+
+   -- Modify your inserted animals to include owner information (owner_id):  
+
+ UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon';
+
+UPDATE animals SET owner_id = 2 WHERE name = 'Pikachu';
+
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon';
+
+UPDATE animals SET owner_id = 3 WHERE name = 'Plantmon';
+
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander';
+
+UPDATE animals SET owner_id = 4 WHERE name = 'Squirtle';
+
+UPDATE animals SET owner_id = 4 WHERE name = 'Blossom';
+
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon';
+
+UPDATE animals SET owner_id = 5 WHERE name = 'Boarmon';
+
+
+
+
--- schema.sql
+++ schema.sql
@@ -11,3 +11,34 @@ CREATE TABLE animals (
     );
 
     ALTER TABLE animals ADD COLUMN species TEXT;
+
+    -- CREATE owners table
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name TEXT,
+    age INT
+);
+
+--CREATE species tabel
+CREATE TABLE spacies(
+ id SERIAL PRIMARY KEY,
+  name TEXT
+);
+ -- Modifay animal table
+ --Make sure that id is set as autoincremented PRIMARY KEY
+  CREATE SEQUENCE animals_id_seq owned BY animals.id;
+
+  ALTER TABLE animals ALTER COLUMN id SET DEFAULT nextval('animals_id_seq');
+
+  SELECT SETVAL(pg_get_serial_sequence('animals', 'id'), max(id)) FROM animals;
+
+--Remove column species
+ALTER TABLE animals DROP COLUMN species;
+
+--Add column species_id which is a foreign key referencing species table
+
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species;
+
+--Add column owner_id which is a foreign key referencing the owners table
+    ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners;
+
