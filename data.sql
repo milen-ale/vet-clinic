05c6ea05c20bd745d291a166bb3403c631bf8b84
milen-ale <milenalebel2@gmail.com>
2022-10-21 06:25:10 -0700

    update readme  file

--- data.sql
+++ data.sql
@@ -1,5 +1,152 @@
 /* Populate database with sample data. */
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8);
+
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
+
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-05-12', 5, TRUE, 11);
+
+INSERT INTO animals(id, name,date_of_birth,escape_attemtps, neutered, weight_kg) VALUES ('Charmander', '2020-02-08', 2, FALSE, -11)
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (6, 'Plantmon', '2021-11-15', 2, TRUE, -5.7);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (7, 'Squirtle', '1993-04-02', 3, FALSE, -12.13);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (8, 'Angemon', '2005-06-12', 1, TRUE, -45);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (9, 'Boarmon', '2005-06-07', 7, TRUE, 20.4);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (10, 'Blossom', '1998-10-13', 3, TRUE, 17);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+--INSERT data into owner table
+INSERT INTO owners(id, full_name, age) VALUES (1, sam smith, 31);
+INSERT INTO owners (id, full_name, age) VALUES (Jennifer Orwell, 19);
+INSERT INTO owners (id, full_name, age) VALUES (Melody Pond, 77);
+INSERT INTO owners (id, full_name, age) VALUES (Dean Winchester , 14);
+INSERT INTO owners (id, full_name, age) VALUES (5,Jodie Whittaker, 38);
+
+--INSERT data into species table
+   INSERT INTO species (name) VALUES ('Pokemon');
+
+   INSERT INTO species (name) VALUES ('Digimon');
+
+   INSERT INTO species (name) VALUES ('Pokemon'),('Digimon');
+
+--Modify your inserted animals so it includes the species_id value
+  --If the name ends in "mon" it will be Digimon
+     UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+     --All other animals are Pokemon
+     UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+
+   -- Modify your inserted animals to include owner information (owner_id):  
+
+ UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon';
+
+UPDATE animals SET owner_id = 2 WHERE name = 'Pikachu';
+
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon';
+
+UPDATE animals SET owner_id = 3 WHERE name = 'Plantmon';
+
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander';
+
+UPDATE animals SET owner_id = 4 WHERE name = 'Squirtle';
+
+UPDATE animals SET owner_id = 4 WHERE name = 'Blossom';
+
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon';
+
+UPDATE animals SET owner_id = 5 WHERE name = 'Boarmon';
+
+-- Insert Data for Vets
+INSERT INTO vets (name, age, date_of_graduation)  
+VALUES                                     
+('William Tatcher', 45, DATE '2000-04-23'),
+('Maisy Smith', 26, DATE '2019-01-17'),
+('Stephanie Mendez', 64, DATE '1981-05-04'),
+('Jack Harkness', 38, DATE '2008-06-08');
+
+
+--Insert the following data for specialties
+--/* Vet William Tatcher is specialized in Pokemon. Vet Stephanie Mendez is specialized in Digimon and Pokemon. Vet Jack Harkness is specialized in Digimon. */
+
+INSERT INTO specializations (species_id, vets_id)
+ VALUES ((SELECT id from spacies WHERE name = 'Pokemon'),
+ (SELECT id FROM vets WHERE name = 'William Tatcher')), 
+ ((SELECT id from spacies WHERE name = 'Digimon'), 
+ (SELECT id FROM vets WHERE name = 'Stephanie Mendez')),
+((SELECT id from spacies WHERE name = 'Pokemon'), 
+(SELECT id FROM vets WHERE name = 'Stephanie Mendez')),
+((SELECT id from spacies WHERE name = 'Digimon'),
+ (SELECT id FROM vets WHERE name = 'Jack Harkness'));
+
+-- Insert the following data for visits:
+
+-- Agumon visited William Tatcher on May 24th, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2020-05-24');
+
+-- Agumon visited Stephanie Mendez on Jul 22th, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-07-22');
+
+-- Gabumon visited Jack Harkness on Feb 2nd, 2021.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Gabumon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2021-02-02');
+
+-- Pikachu visited Maisy Smith on Jan 5th, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-01-05');
+
+-- Pikachu visited Maisy Smith on Mar 8th, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-03-08');
+
+-- Pikachu visited Maisy Smith on May 14th, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-05-14');
+
+-- Devimon visited Stephanie Mendez on May 4th, 2021.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Devimon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2021-05-04');
+
+-- Charmander visited Jack Harkness on Feb 24th, 2021.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Charmander'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2021-02-24');
+
+-- Plantmon visited Maisy Smith on Dec 21st, 2019.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Plantmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2019-12-21');
+
+-- Plantmon visited William Tatcher on Aug 10th, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Plantmon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2020-08-10');
+
+-- Plantmon visited Maisy Smith on Apr 7th, 2021.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Plantmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2021-04-07');
+
+-- Squirtle visited Stephanie Mendez on Sep 29th, 2019.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Squirtle'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2019-09-29');
+
+-- Angemon visited Jack Harkness on Oct 3rd, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Angemon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2020-10-03');
+
+-- Angemon visited Jack Harkness on Nov 4th, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Angemon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2020-11-04');
+
+-- Boarmon visited Maisy Smith on Jan 24th, 2019.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2019-01-24');
+
+-- Boarmon visited Maisy Smith on May 15th, 2019.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2019-05-15');
+
+-- Boarmon visited Maisy Smith on Feb 27th, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-02-27');
+
+-- Boarmon visited Maisy Smith on Aug 3rd, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-08-03');
+
+-- Blossom visited Stephanie Mendez on May 24th, 2020.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-05-24');
+
+-- Blossom visited William Tatcher on Jan 11th, 2021.
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id from animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11');
+
+ 
+
+ 
--- schema.sql
+++ schema.sql
@@ -1,5 +1,67 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id integer primary key,
+    name varchar(100),
+    date_of_birth date,
+    escape_attemps integer,
+    neutered boolean,
+    weight_kg decimal
+    
+    );
+
+    ALTER TABLE animals ADD COLUMN species TEXT;
+
+    -- CREATE owners table
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name TEXT,
+    age INT
 );
+
+--CREATE species tabel
+CREATE TABLE spacies(
+ id SERIAL PRIMARY KEY,
+  name TEXT
+);
+ -- Modifay animal table
+ --Make sure that id is set as autoincremented PRIMARY KEY
+  CREATE SEQUENCE animals_id_seq owned BY animals.id;
+
+  ALTER TABLE animals ALTER COLUMN id SET DEFAULT nextval('animals_id_seq');
+
+  SELECT SETVAL(pg_get_serial_sequence('animals', 'id'), max(id)) FROM animals;
+
+--Remove column species
+ALTER TABLE animals DROP COLUMN species;
+
+--Add column species_id which is a foreign key referencing species table
+
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species;
+
+--Add column owner_id which is a foreign key referencing the owners table
+    ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners;
+
+--Create a table named vets
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150),
+    age INT,
+    date_of_graduation DATE
+);
+
+-- There is a many-to-many relationship between the tables species and vets: a vet can specialize in multiple species, and a species can have multiple vets specialized in it. Create a "join table" called specializations to handle this relationship.
+
+CREATE TABLE specializations (
+    species_id INT REFERENCES spacies (id),
+    vets_id INT REFERENCES vets (id)
+);
+
+--There is a many-to-many relationship between the tables animals and vets: an animal can visit multiple vets and one vet can be visited by multiple animals. Create a "join table" called visits to handle this relationship
+
+CREATE TABLE visits (
+    animals_id INT REFERENCES animals (id),
+    vets_id INT REFERENCES vets (id),
+    date_of_visit DATE
+);
+
